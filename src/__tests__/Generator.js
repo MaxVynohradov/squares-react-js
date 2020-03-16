3044411bb0466113e66f57e2fb4c1fd424b2aec0
MaxVinohradov <vinogradov.max97@gmail.com>
2020-03-17 00:54:51 +0200

    tests(generator): check with custom props

--- src/__tests__/Generator.js
+++ src/__tests__/Generator.js
@@ -8,8 +8,20 @@ describe('Basic Generator Functionality', () => {
   it('should generate default table', () => {
     const trsWrapper = mount(<Generator />).find(Table).find('tr');
     expect(trsWrapper).toHaveLength(4);
-    trsWrapper.forEach((node) => {
-      expect(node.find('td')).toHaveLength(4);
-    })
+    trsWrapper.forEach((node) => expect(node.find('td')).toHaveLength(4));
   })
+
+  it.each([
+    [2, 2], [2, 3], [3, 5], [9, 1], [1, 4]
+  ])(
+    'should generate table with props: width - %s, height = %s', 
+    (height, width) => {
+      const trsWrapper = mount(
+        <Generator initialWidth={width} initialHeight={height} />
+      ).find(Table).find('tr');
+      expect(trsWrapper).toHaveLength(height);
+      trsWrapper.forEach((node) => expect(node.find('td')).toHaveLength(width));  
+    }
+  );
+
 });
