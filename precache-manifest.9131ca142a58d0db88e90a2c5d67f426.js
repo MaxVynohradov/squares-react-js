1a7a2badda5e683cadc5a2c2d269cd77829bcefe
MaxVinogradov <vinogradov.max97@gmail.com>
2020-03-16 23:45:41 +0000

    Deploying to gh-pages from master 699553d8fae2ec9bda244d18a6021f4d0d1aa1a3

---
+++ precache-manifest.9131ca142a58d0db88e90a2c5d67f426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadc37b149fd2bd63c2e1ec0c0fdc017",
+    "url": "/squares-react-js/index.html"
+  },
+  {
+    "revision": "252bfa40ab5710575053",
+    "url": "/squares-react-js/static/css/main.a5e39e8f.chunk.css"
+  },
+  {
+    "revision": "30e5b009b9ded0aa7c83",
+    "url": "/squares-react-js/static/js/2.9034ca46.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/squares-react-js/static/js/2.9034ca46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "252bfa40ab5710575053",
+    "url": "/squares-react-js/static/js/main.f775b7e6.chunk.js"
+  },
+  {
+    "revision": "6c4f23bcc377b417fe08",
+    "url": "/squares-react-js/static/js/runtime-main.ae9eaa11.js"
+  }
+]);
